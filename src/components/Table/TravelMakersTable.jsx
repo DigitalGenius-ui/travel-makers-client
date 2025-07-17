0322dde3067468efee588215535be259089e0e8e
DigitalGenius-ui <miladmarketing282@gmail.com>
2025-07-17 22:23:29 +0800

    auto suggestion is enabled

--- src/components/Table/TravelMakersTable.jsx
+++ src/components/Table/TravelMakersTable.jsx
@@ -1,6 +1,16 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { MaterialReactTable } from "material-react-table";
 
+// to get the auto suggestion
+
+/**
+ * @template T
+ * @param {{
+ *   columns: import('material-react-table').MRT_ColumnDef<T>[],
+ *   data: T[],
+ * } & Partial<import('material-react-table').MRT_TableOptions<T>>} props
+ */
+
 const TravleMakersTable = ({ columns, data, ...others }) => {
   const tableTheme = createTheme({
     palette: {
